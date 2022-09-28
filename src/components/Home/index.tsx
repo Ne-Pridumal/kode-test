737bd016a019ce4be2f7b89dc694d595899ff04d
Ne-Pridumal <ivan.gavrilkov@gmail.com>
2022-09-28 10:02:52 +0200

    start creating components

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
+import SearchInput from "../SearchInput";
+import './index.css'
 
 const Home: FC = () => {
   return (
-    <div>test</div>
+    <div className="home">
+      <h2 className="home__title">
+        Поиск
+      </h2>
+      <SearchInput />
+    </div>
   )
 }
 
---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const SearchInput: FC = () => {
+  return (
+    <div className="search-container">
+      <input className="search__input" />
+    </div>
+  )
+}
+export default SearchInput
